9b98d2c86362ea10cfc932d8a5341b1064cd3735
lordyppopdy-lab <deelordpopdy2@gmail.com>
2025-10-07 09:32:38 -0700

    safe

--- tools_Facebook/server/index.js
+++ tools_Facebook/server/index.js
@@ -7,10 +7,10 @@ app.use(express.json());
 app.use("/", require("./routes/authRouter"))
 
 mongoose.connect(process.env.MONGO_URL)
- .then(()=>console.log("FB_NightPlayer Database Connected!"))
- .catch((err) => console.log("Error Connecting FB_Night Player!", err ))
+    .then(() => console.log("FB_NightPlayer Database Connected!"))
+    .catch((err) => console.log("Error Connecting FB_Nights Player!", err))
 
 const PORT = 4000;
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Night player is running at port ${PORT}`);
 })
